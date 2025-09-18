5b7290982a21ed721c9024004d57835e7256baba
Mohammed abdul bari waseem <mohdabdulbariwaseem@gmai.com>
2025-09-17 21:44:57 -0400

    pathvaiables upto {id}

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,31 @@ public class HelloWorldController {
 	return "Hello World";
 			
 	}
+	
+	
+	
+	
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean HelloWorldBean(){
+		
+	return new  HelloWorldBean ("Hello World");
+			
+	}
+	
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean HelloWorldBeanVariable(@PathVariable String name){
+		
+	return new  HelloWorldBean (String.format("Hello World, %s", name));
+			
+	}
+	
+	
+	
+	
+	
+	
 	
 }
 	
